657d96b92c5f0fe58f2b2e36871ddc4714b9eed8
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-09-21 17:17:35 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [inputs, setInputs] = useState();
+  const [list, setList] = useState([]);
+  const no = useRef(list.length + 1);
+
+  const onChange = (e) => {
+    setInputs(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setInputs("");
+    setList([
+      ...list,
+      {
+        id: no.current++,
+        text: inputs,
+      },
+    ]);
+    console.log("list : ", list);
+  };
+
+  const onDelete = (id) => {
+    setList(list.filter((list) => list.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Practice </h1>
+
+      <form onSubmit={onSubmit}>
+        <input required placeholder="here" onChange={onChange} value={inputs}></input>
+        <button type="submit">Ok</button>
+      </form>
+
+      <h1>List</h1>
+
+      {list.map((text) => (
+        <li>
+          {text.text}
+          <button onClick={() => onDelete(text.id)}>Del</button>
+        </li>
+      ))}
     </div>
   );
 }
